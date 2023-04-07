a51d93b9980fcd5422d1b77053187805149afb05
Kanishk Shrivastava <65497298+knixk@users.noreply.github.com>
2023-04-07 16:04:06 +0000

    added flask project

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+  return "<p>Hello, World!</p>"
+
+
+print(__name__)
+
+if __name__ == "__main__":
+  # print("inside the main now")
+  app.run(
+    host='0.0.0.0',
+    debug=True,
+  )
